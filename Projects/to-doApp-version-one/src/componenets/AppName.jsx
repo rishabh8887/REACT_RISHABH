e3335eb84662a5e4ca6258305ee9e842ccaed751
Rishabh Bajpai <rishabhbajpai131@gmail.com>
2026-02-19 21:36:57 +0530

    kg-coding

--- Projects/to-doApp-version-one/src/componenets/AppName.jsx
+++
@@ -1,4 +0,0 @@
-function AppName(){
-return <h1 className="nameofapp">Todo React App</h1>
-}
-export default AppName;
---
+++ kg_coding/Projects/to-do-v1/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <center className="todo-container">placeholder</center>
+      <h1>To do App</h1>
+    </>
+  );
+}
+export default App;

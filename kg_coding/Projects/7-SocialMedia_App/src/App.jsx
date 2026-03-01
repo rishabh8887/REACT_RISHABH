2f87320630bdabbdd17fe26a64354d0fc50924ff
Rishabh Bajpai <rishabhbajpai131@gmail.com>
2026-03-01 21:06:41 +0530

    UI configration done

--- kg_coding/Projects/7-SocialMedia_App/src/App.jsx
+++ kg_coding/Projects/7-SocialMedia_App/src/App.jsx
@@ -1,14 +1,18 @@
 import "./App.css";
+import CreatePost from "./components/Createost";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import PostList from "./components/PostList";
 import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <>
       <div className="app-container">
         <Sidebar />
-        <div>
+        <div className="content">
           <Header />
+          <CreatePost />
+          <PostList />
           <Footer />
         </div>
       </div>

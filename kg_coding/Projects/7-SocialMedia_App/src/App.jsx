ec5547cc65d0af9fccd5a6b30a38c8f1558dd93d
Rishabh Bajpai <rishabhbajpai131@gmail.com>
2026-03-01 19:11:37 +0530

    social media app setup

---
+++ kg_coding/Projects/7-SocialMedia_App/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+function App() {
+  return (
+    <>
+      <div className="app-container">
+        <Sidebar />
+        <div>
+          <Header />
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ kg_coding/Projects/7-SocialMedia_App/src/components/Sidebar.jsx
@@ -0,0 +1,57 @@
+const Sidebar = () => {
+  return (
+    <>
+      <div
+        className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark"
+        style={{ width: "280px" }}
+      >
+        <a
+          href="/"
+          className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
+        >
+          <svg
+            className="bi pe-none me-2"
+            width="40"
+            height="32"
+            aria-hidden="true"
+          >
+            <use xlinkHref="#bootstrap"></use>
+          </svg>
+          <span className="fs-4">Sidebar</span>
+        </a>
+
+        <hr />
+
+        <ul className="nav nav-pills flex-column mb-auto">
+          <li className="nav-item">
+            <a href="#" className="nav-link active" aria-current="page">
+              Home
+            </a>
+          </li>
+        </ul>
+
+        <hr />
+
+        <div className="dropdown">
+          <a
+            href="#"
+            className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <img
+              src="https://github.com/mdo.png"
+              alt=""
+              width="32"
+              height="32"
+              className="rounded-circle me-2"
+            />
+            <strong>mdo</strong>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;

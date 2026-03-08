c6c523e3980d16022c8610a5868636966ad0e521
Rishabh Bajpai <rishabhbajpai131@gmail.com>
2026-03-08 11:12:58 +0530

    new project initialized for advance concept leaarning

---
+++ kg_coding/Projects/8-React-Advance/src/components/PostList.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { PostList as PostListData } from "../store/post-list-store";
+import Post from "./Post";
+
+const PostList = () => {
+  const { postList } = useContext(PostListData);
+  return (
+    <>
+      {postList.map((post) => {
+        return <Post key={post.id} post={post} />;
+      })}
+    </>
+  );
+};
+export default PostList;
---
+++ kg_coding/Projects/8-React-Advance/src/store/post-list-store.jsx
@@ -0,0 +1,74 @@
+import { createContext, useReducer } from "react";
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+const postListReducer = (currPostList, action) => {
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.payload.postId,
+    );
+  }else if(action.type === 'Add_POST'){
+    newPostList = [action.payload,...currPostList]
+  }
+
+  return newPostList;
+};
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST,
+  );
+
+  const addPost = (userId, postTitle, postBody, reactions, tags) => {
+    dispatchPostList({
+      type: "Add_POST",
+      payload: {
+          id: Date.now(),
+        title: postTitle,
+        body: postBody,
+        reaction: reactions,
+        userId: userId,
+        tags: tags,
+      },
+    });
+  };
+
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: {
+        postId,
+      },
+    });
+  };
+
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going to Mumbai",
+    body: "Hi friends i am going to mumbai for my vacations hope to enjoy a lot Peace out",
+    reaction: 2,
+    userId: "user-9",
+    tags: ["vacation", "Mumbai", "Enjoying"],
+  },
+  {
+    id: "2",
+    title: "Pass ho gye bhai",
+    body: "4 saal ki masti ke baad bhi ho gye h pass",
+    reaction: 15,
+    userId: "user-9",
+    tags: ["Graduating", "Unbelieveable"],
+  },
+];
+
+export default PostListProvider;

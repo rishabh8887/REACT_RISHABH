653e229ce86a9a1c36da64452e3060f6aaed5725
Rishabh Bajpai <rishabhbajpai131@gmail.com>
2026-03-11 14:33:21 +0530

    redux done with counter project

--- kg_coding/52_Redux/Counter-React-Redux/src/App.jsx
+++ kg_coding/52_Redux/Counter-React-Redux/src/App.jsx
@@ -1,17 +1,20 @@
+import { useSelector } from "react-redux";
 import "./App.css";
 import Container from "./components/Container";
 import Controls from "./components/Controls";
 import DispplayCounter from "./components/DisplayCounter";
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import PrivacyMessage from "./components/PrivacyMessage";
 
 function App() {
+  const privacy = useSelector((store) => store.privacy);
   return (
     <center className="px-4 py-5 my-5 text-center">
       <Container>
         <Header />
         <div className="col-lg-6 mx-auto">
-          <DispplayCounter />
+          {privacy ? <PrivacyMessage /> : <DispplayCounter />}
           <Controls />
         </div>
       </Container>
--- kg_coding/52_Redux/Counter-React-Redux/src/components/Controls.jsx
+++ kg_coding/52_Redux/Counter-React-Redux/src/components/Controls.jsx
@@ -1,13 +1,41 @@
+import { useRef } from "react";
 import { useDispatch } from "react-redux";
 
 const Controls = () => {
   const dispatch = useDispatch();
+  const inputElement = useRef();
 
   const handleIncrement = () => {
     dispatch({ type: "INCREMENT" });
   };
   const handleDecrement = () => {
     dispatch({ type: "DECREMENT" });
+  };
+  const handleAdd = () => {
+    dispatch({
+      type: "ADD",
+      payload: {
+        num: inputElement.current.value,
+      },
+    });
+    inputElement.current.value = "";
+  };
+
+  const handleSubtract = () => {
+    dispatch({
+      type: "SUBTRACT",
+      payload: {
+        num: inputElement.current.value,
+      },
+    });
+    inputElement.current.value = "";
+  };
+
+  const handleHide = () => {
+    dispatch({
+      type: "HIDE",
+    });
+    inputElement.current.value = "";
   };
 
   return (
@@ -22,10 +50,39 @@ const Controls = () => {
         </button>
         <button
           type="button"
-          className="btn btn-outline-secondary "
+          className="btn btn-secondary "
           onClick={handleDecrement}
         >
           DECREMENT -1
+        </button>
+      </div>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center  control-row">
+        <input
+          type="text"
+          className="number-input"
+          placeholder="Enter number"
+          ref={inputElement}
+        />
+
+        <button type="button" className="btn btn-info " onClick={handleAdd}>
+          ADD
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-danger "
+          onClick={handleSubtract}
+        >
+          SUBTRACT
+        </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          className="btn btn-warning control-row-hide"
+          onClick={handleHide}
+        >
+          PRIVACY TOGGLE
         </button>
       </div>
     </>
